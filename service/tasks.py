9729363053d7dd0906dd2515610bc3e6c4f20252
Grigory <lololohka057@gmail.com>
2024-01-20 14:36:06 +0300

    finish

--- service/tasks.py
+++ service/tasks.py
@@ -1,46 +1,40 @@
 from django.core.mail import send_mail
+import pytz
 from project import settings
 from service.models import Mailing_Logs, Message_to_Send, Settings
 from django.utils import timezone
 from django.db.models import Q
-from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 from celery import shared_task
 from datetime import datetime, timedelta
 
 
 @shared_task
-def send_mailing_task(mailing):
-   settings_objects = Settings.objects.all()
-   recipients = settings_objects.values_list('client', flat=True)
-   print(recipients)
+def send_mailing_task(mailing, message, current_date_time, recipient):
    subject = mailing.letter_subject
    message = mailing.letter_body
 
-   for recipient in recipients:
-      recipient_email = recipient.email
-
-      try:
-         send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[recipient_email])
+   try:
+      send_mail(
+         subject=subject,
+         message=message,
+         from_email=settings.EMAIL_HOST_USER,
+         recipient_list=[recipient.email])
          
-         log = Mailing_Logs(
-            date_and_time_of_last_attempt=timezone.now(),
-            attempt_status="Success",
-            mail_server_response="Success",
-            settings=Settings.objects.filter(client_id=recipient.pk))
-         log.save()
+      log = Mailing_Logs(
+         date_and_time_of_last_attempt=current_date_time,
+         attempt_status="Success",
+         mail_server_response="Success",
+         settings=Settings.objects.filter(client_id=recipient.pk))
+      log.save()
 
-      except Exception as e:
-         log = Mailing_Logs(
-            date_and_time_of_last_attempt=timezone.now(),
-            attempt_status="Error",
-            mail_server_response=e,
-            settings=Settings.objects.filter(client_id=recipient.pk))
-         log.save()
+   except Exception as e:
+      log = Mailing_Logs(
+         date_and_time_of_last_attempt=current_date_time,
+         attempt_status="Error",
+         mail_server_response=e,
+         settings=Settings.objects.filter(client_id=recipient.pk))
+      log.save()
 
    mailing.save()
 
@@ -53,30 +47,27 @@ def start_mailings():
    Периодичность отправки рассылок зависит от настроек каждой рассылки.
    """
    
-   current_time = datetime.now().replace(second=0, microsecond=0)
-   # Фильтруем объекты Mailing, чтобы найти те, которые имеют статус Mailing.STATUS_CREATED
-   # или Mailing.STATUS_STARTED и у которых время рассылки меньше или равно текущему времени.
-   settings_objects = Settings.objects.filter(Q(mailing_status="создана") | Q(mailing_status="запущена"), mailing_time_date__lte=current_time)
+   zone = pytz.timezone(settings.TIME_ZONE)
+   current_date_time = datetime.now(zone)
 
-   scheduled_mailings = Message_to_Send.objects.filter(settings__in=settings_objects)
+   mailings = Settings.objects.filter(mailing_time_date__lte=current_date_time)
 
-   for mailing in scheduled_mailings:
-      setting = mailing.settings
-      setting.mailing_status = "запущена"
+   for mailing in mailings:
 
-      setting.save()
-      send_mailing_task.delay(mailing)
+      message = Message_to_Send.objects.get(settings=mailing)
+      recipient = mailing.client
+      print(recipient)
+      send_mailing_task.delay(mailing, message, current_date_time, recipient)
 
+      if mailing.periodicity == "раз в день":
+         mailing.mailing_time_date += timedelta(days=1)
+      elif mailing.periodicity == "раз в неделю":
+         mailing.mailing_time_date += timedelta(weeks=1)
+      elif mailing.periodicity == "раз в месяц":
+         mailing.mailing_time_date += relativedelta(months=1)
 
-      if setting.periodicity == "раз в день":
-         setting.mailing_time_date += timedelta(days=1)
-      elif setting.periodicity == "раз в неделю":
-         setting.mailing_time_date += timedelta(weeks=1)
-      elif setting.periodicity == "раз в месяц":
-         setting.mailing_time_date += relativedelta(months=1)
+      if mailing.mailing_time_date.day > 28:
+         mailing.mailing_time_date = mailing.mailing_time_date.replace(day=1)
+         mailing.mailing_time_date += relativedelta(day=31)
 
-      if setting.mailing_time_date.day > 28:
-         setting.mailing_time_date = mailing.mailing_time_date.replace(day=1)
-         setting.mailing_time_date += relativedelta(day=31)
-
-      setting.save()
+      mailing.save()
